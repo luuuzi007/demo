189cff550c2e5819389510f24dc0f2ea7b24f029
wangxinglu <1143303231@qq.com>
2020-02-03 20:07:17 +0800

    添加arouter路由

--- app/src/main/java/com/example/demo006/MyApplication.java
+++ app/src/main/java/com/example/demo006/MyApplication.java
@@ -9,7 +9,7 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        if (BuildConfig.DEBUG){
+        if (BuildConfig.DEBUG){//debug状态下才打印日志
             ARouter.openLog();//打印日志
             ARouter.openDebug();//开启调试模式(如果在InstantRun模式下运行，必须开启调试模式！线上版本需要关闭,否则有安全风险)
         }
